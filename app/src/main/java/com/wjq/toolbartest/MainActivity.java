5899f8944e1d540323c247de65f994818849523b
JackQueen <457883454@qq.com>
2017-04-19 10:23:04 +0800

    test finish

--- app/src/main/java/com/wjq/toolbartest/MainActivity.java
+++ app/src/main/java/com/wjq/toolbartest/MainActivity.java
@@ -2,12 +2,41 @@ package com.wjq.toolbartest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        toolbar.setTitle("Toolbar主标题");//设置toolbar 标题
+        toolbar.setSubtitle("Toolbar子标题");//设置toolbar子标题
+        toolbar.inflateMenu(R.menu.main);
+        ActionMenuView acmenu = (ActionMenuView) findViewById(R.id.action_menu);
+        getMenuInflater().inflate(R.menu.main,acmenu.getMenu());
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "点击导航栏", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
